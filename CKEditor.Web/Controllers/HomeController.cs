ce3f9e4b2ce35c23f854bc0d5f4dd6b56c7e0b84
hamanshareghi <haman.shareghi@gmail.com>
2020-09-13 02:03:38 +0430

    Add JS Files And Changes

--- CKEditor.Web/Controllers/HomeController.cs
+++ CKEditor.Web/Controllers/HomeController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CKEditor.Web.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace CKEditor.Web.Controllers
 {
@@ -21,6 +23,41 @@ namespace CKEditor.Web.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile fileUp, string CKEditorFuncNum, string CKEditor,
+            string langCode)
+        {
+            string filePath = String.Empty;
+            string vMessage = String.Empty;
+            //string vFilePath = String.Empty;
+            string vOutput = String.Empty;
+            try
+            {
+                if (fileUp != null)
+                {
+                    var fileName = Guid.NewGuid() + Path.GetExtension(fileUp.FileName);
+                    var directory = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Upload/");
+                    filePath = Path.Combine(directory, fileName);
+
+                    if (!Directory.Exists(directory))
+                    {
+                        Directory.CreateDirectory(directory);
+                    }
+
+                    await using var stream = new FileStream(filePath, FileMode.Create);
+                    await fileUp.CopyToAsync(stream);
+                    filePath = Url.Content("/Upload/" + fileName);
+                    vMessage = "تصویر با مفقیت ذخیره شد";
+                }
+            }
+            catch
+            {
+                vMessage = "There was an issue uploading";
+            }
+            vOutput = @"<html><body><script>window.parent.CKEDITOR.tools.callFunction(" + CKEditorFuncNum + ", \"" + filePath + "\", \"" + vMessage + "\");</script></body></html>";
+            return Content(vOutput);
         }
 
         public IActionResult Privacy()
