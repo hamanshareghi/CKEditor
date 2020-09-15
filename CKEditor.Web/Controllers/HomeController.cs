730cea21f41cc5c3b8c144571db67d7f8c0404be
hamanshareghi <haman.shareghi@gmail.com>
2020-09-15 11:00:17 +0430

    Ckeditor With Upload Image

--- CKEditor.Web/Controllers/HomeController.cs
+++ CKEditor.Web/Controllers/HomeController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CKEditor.Web.Models;
@@ -25,50 +22,29 @@ namespace CKEditor.Web.Controllers
             return View();
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Upload(IFormFile fileUp, string CKEditorFuncNum, string CKEditor,
-            string langCode)
-        {
-            string filePath = String.Empty;
-            string vMessage = String.Empty;
-            //string vFilePath = String.Empty;
-            string vOutput = String.Empty;
-            try
-            {
-                if (fileUp != null)
-                {
-                    var fileName = Guid.NewGuid() + Path.GetExtension(fileUp.FileName);
-                    var directory = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Upload/");
-                    filePath = Path.Combine(directory, fileName);
-
-                    if (!Directory.Exists(directory))
-                    {
-                        Directory.CreateDirectory(directory);
-                    }
-
-                    await using var stream = new FileStream(filePath, FileMode.Create);
-                    await fileUp.CopyToAsync(stream);
-                    filePath = Url.Content("/Upload/" + fileName);
-                    vMessage = "تصویر با مفقیت ذخیره شد";
-                }
-            }
-            catch
-            {
-                vMessage = "There was an issue uploading";
-            }
-            vOutput = @"<html><body><script>window.parent.CKEDITOR.tools.callFunction(" + CKEditorFuncNum + ", \"" + filePath + "\", \"" + vMessage + "\");</script></body></html>";
-            return Content(vOutput);
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        [HttpPost]
+        [Route("file-upload")]
+        public IActionResult UploadImage(IFormFile upload, string CKEditorFuncNum, string CKEditor, string langCode)
+        {
+            if (upload.Length <= 0) return null;
+            var fileName = Guid.NewGuid() + Path.GetExtension(upload.FileName);
+            var path = Path.Combine(
+                Directory.GetCurrentDirectory(), "wwwroot/Uploads",
+                fileName);
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                upload.CopyTo(stream);
+
+            }
+            var url = $"/Uploads/{fileName}";
+            return Json(new { uploaded = true, url });
+        }
+
     }
 }
